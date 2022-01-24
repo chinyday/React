d857ecc0e19a1abc51254037d732dfddf0ed3dfc
chinyday <47979888+chinyday@users.noreply.github.com>
2022-01-24 14:37:28 +0900

    todo 입력받아서 배열에 넣기

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };  
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") return;
+    setTodos((curAtty) => [todo, ...curAtty]);
+    setTodo("");
+  }
+
+  console.log(todos);
+
   return (
-    <div></div>
+    <div>
+      <h2>my to dos ({todos.length})</h2>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} type="text" placeholder="write your to do" />    
+      </form>
+      <button type="sumit" onClick={onSubmit}>add text</button>
+    </div>
   );
 }
 
